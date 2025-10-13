6cce22fcd2e6803bd948fbcf53fcea1123a022da
alex <devkral@web.de>
2025-10-13 04:47:40 +0200

    safety improvements and tests
    Changes:
    - make compose more safe (localhost binds!)
    - bump to postgres 18
    - test harder against middleware and permissions (there is somewhere a
      bug)

---
+++ tests/middleware/test_lilya_authentication.py
@@ -0,0 +1,73 @@
+from lilya.authentication import AuthCredentials, AuthenticationBackend, AuthResult, BasicUser
+from lilya.middleware import DefineMiddleware
+from lilya.middleware.authentication import AuthenticationMiddleware
+from lilya.requests import Connection
+
+from ravyn import Include, Request, get, status
+from ravyn.responses import PlainText
+from ravyn.testclient import create_client
+
+dummy = BasicUser("Dummy")
+
+
+class DenyNotAllowAll(AuthenticationBackend):
+    async def authenticate(self, connection: Connection) -> AuthResult | None:
+        if connection.headers.get("allow-all") == "yes":
+            return (AuthCredentials(["authenticated"]), dummy)
+        return None
+
+
+def test_auth_app_level(test_client_factory):
+    @get(path="/")
+    async def homepage(request: Request) -> PlainText:
+        if request.headers.get("allow-all") == "yes":
+            return request.user.display_name
+        else:
+            raise Exception("Should not be reached")
+
+    with create_client(
+        routes=[homepage],
+        middleware=[DefineMiddleware(AuthenticationMiddleware, backend=[DenyNotAllowAll()])],
+    ) as client:
+        response = client.get("/", headers={"allow-all": "yes"})
+        assert response.status_code == status.HTTP_200_OK
+        assert response.text == "Dummy"
+
+        response = client.get("/")
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+
+
+def test_auth_include_level(test_client_factory):
+    @get(path="/login")
+    async def login(request: Request) -> PlainText:
+        return "logged in"
+
+    @get(path="/")
+    async def homepage(request: Request) -> PlainText:
+        if request.headers.get("allow-all") == "yes":
+            return request.user.display_name
+        else:
+            raise Exception("Should not be reached")
+
+    with create_client(
+        routes=[
+            login,
+            Include(
+                "",
+                routes=[homepage],
+                middleware=[
+                    DefineMiddleware(AuthenticationMiddleware, backend=[DenyNotAllowAll()])
+                ],
+            ),
+        ],
+    ) as client:
+        response = client.get("/login")
+        assert response.status_code == status.HTTP_200_OK
+        assert response.text == "logged in"
+
+        response = client.get("/", headers={"allow-all": "yes"})
+        assert response.status_code == status.HTTP_200_OK
+        assert response.text == "Dummy"
+
+        response = client.get("/")
+        assert response.status_code == status.HTTP_403_FORBIDDEN
