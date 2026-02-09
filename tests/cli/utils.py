d0fc91c71711b4a8707480832757d6254db13926
tarsil <tiago.arasilva@gmail.com>
2026-02-09 10:36:45 +0100

    Fix cli tests

--- tests/cli/utils.py
+++ tests/cli/utils.py
@@ -5,7 +5,6 @@ import subprocess
 def run_cmd(app, cmd, is_app=True):
     if is_app:
         os.environ["RAVYN_DEFAULT_APP"] = app
-    cmd = f"hatch --env test run {cmd}"
     process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
     (stdout, stderr) = process.communicate()
     print("\n$ " + cmd)
